7c0308f5fade091ef9f82861d91542943ebe3cb7
ioc23fox <ioc23fox@gmail.com>
2012-03-22 17:19:18 +0200

    RL

---
+++ RL/RL/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RL
+{
+    public class RL
+    {
+        public static RL Zero
+        {
+            get
+            {
+                return new RL();
+            }
+        }
+        public static RL operator +(RL a, RL b)
+        {
+            RL result = RL.Zero;
+            result.Data.AddRange(a.Data);
+            result.Data.AddRange(b.Data);
+            result.Normalize();
+            return result;
+        }
+
+
+        public int Count { get; private set; }
+        public int Sign { get; private set; }
+        public List<int> Data { get; private set; }
+        public string StringValue
+        {
+            get
+            {
+                return Sign + "." + Count + "." + String.Join(".", Data);
+            }
+        }
+        private string goodRLNumber = @"^(?<sign>(:?- )?)(?<numbers>(-?\d+?\.)*?\d+)$";
+
+        public RL()
+        {
+            Count = 0;
+            Sign = 0;
+            Data = new List<int>();
+        }
+        private void Normalize()
+        {
+            this.Count = this.Data.Count();
+            this.Sign = 0;
+            this.Data = this.Data.OrderByDescending(x => x).ToList<int>();
+            for (int i = 0; i < this.Data.Count() - 1; i++)
+            {
+                var first = this.Data[i];
+                var second = this.Data[i + 1];
+                if (first == second)
+                {
+                    this.Data[i]++;
+                    this.Data.Remove(second);
+
+                    i = 0;
+                }
+            }
+            Console.WriteLine("normalized");
+        }
+
+        public RL(string data)
+        {
+            bool isGood = Regex.IsMatch(data, goodRLNumber);
+            if (isGood)
+            {
+                var matchingResult = Regex.Match(data, goodRLNumber);
+                var numbers = matchingResult.Groups["numbers"].Value;
+                var sign = matchingResult.Groups["sign"].Value;
+
+                Data = numbers.Split('.').ToList<string>().ConvertAll<int>(x => int.Parse(x)).OrderByDescending(x => x).ToList<int>();
+                Count = Data.Count();
+                Sign = sign == "- " ? 1 : 0;
+            }
+            else throw new Exception("Error: bad data in " + data);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new RL("14.10.9.8.5.4.3.-1.0");
+            var secod = new RL("17.15.10.6");
+            Console.WriteLine(first.StringValue);
+            Console.WriteLine(secod.StringValue);
+            //var result = first + secod;
+            //Console.WriteLine(result.StringValue);
+           
+            Console.Read();
+        }
+    }
+}
---
+++ RL/RL_UnitTest/RLTest.cs
@@ -0,0 +1,91 @@
+﻿using RL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RL_UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for RLTest and is intended
+    ///to contain all RLTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class RLTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        public static string[,] testData = new string[,]
+            {
+                {"10.12.5.2", "15.10.3", "15.12.11.5.3.2"},
+                {"3.2.5.10.11", "3.15.9.2", "15.11.10.9.5.4.3"}
+            };
+
+        /// <summary>
+        ///A test for op_Addition
+        ///</summary>
+        [TestMethod()]
+        public void op_AdditionTest()
+        {
+            for (int i = 0; i < testData.Length; i++)
+            {
+                RL.RL a = new RL.RL(testData[i, 0]);
+                RL.RL b = new RL.RL(testData[i, 1]);
+                RL.RL expected = new RL.RL(testData[i, 2]);
+                RL.RL actual;
+                actual = (a + b);
+                Assert.AreEqual(expected.StringValue, actual.StringValue);
+            }
+            //Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
